eedbb539299cdb6345e5548cbc9ca024154b8b6c
cyberspacedk <frontend.dk@gmail.com>
2020-01-17 10:07:48 +0200

    get access to device camera

--- great_places/components/ImagePicker/component.js
+++ great_places/components/ImagePicker/component.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Alert} from 'react-native';
 import * as Picker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 
-import {Container, ImageContainer, ImageLabel, UploadButton} from './styles';
+import {Container, ImageContainer, ImageLabel, Image, UploadButton} from './styles';
 
 import Colors from '../../constants/colors';
 
 const ImagePicker = () => {
+  const [pickedImage, setPicketImage] = useState(null);
 
   const verifyPermissions = async ()=>{
    const result =  await Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL);
@@ -18,21 +19,30 @@ const ImagePicker = () => {
       'You need to grant camera permissions to use this app.',
       [{test: 'Okay'}]
      );
-     return true;
+     return false;
     }
-    return false;
+    return true;
   };
 
   const pickImage = async () => {
     const granted = await verifyPermissions();
     if(!granted) return;
-    await Picker.launchCameraAsync();
+    const image = await Picker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16,9],
+      quality: 0.5
+    });
+    setPicketImage(image.uri) 
   };
 
   return (
     <Container>
       <ImageContainer>
-        <ImageLabel>No image picked yet.</ImageLabel>
+        {pickedImage ? 
+          <Image source={{uri: pickedImage}} />
+        :
+          <ImageLabel>No image picked yet.</ImageLabel> 
+        }
       </ImageContainer>
       <UploadButton 
         title="Upload image"
